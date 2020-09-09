956942411deebba7787aeb72c7351bb0a8d401e1
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-09-09 12:33:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 function addingEventListener() {
+  //node that is listening for the click event is being stored in input 
   const input = document.getElementById('input');
   input.addEventListener('click', function(event) {
     alert('I was clicked!');
